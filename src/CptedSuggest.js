ca690100c4a03e739954fe566f27de6b001380bb
nongdamgom1over <phs1289@gmail.com>
2024-08-29 20:59:13 +0900

    8/29 로컬 맵레이어 띄우기

--- src/CptedSuggest.js
+++ src/CptedSuggest.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./CptedSuggest.css";
 
-const CptedSuggest = ({ clickedAddress }) => {
+const CptedSuggest = ({ clickedAddress, selectedFeature }) => {
     const navigate = useNavigate();
+    const [crimeRanks, setCrimeRanks] = useState([]);
+
+    useEffect(() => {
+        if (selectedFeature) {
+            const crviol = selectedFeature.get('CRVIOL');
+            const crtheft = selectedFeature.get('CRTHEFT');
+            const crsex = selectedFeature.get('CRSEX');
+
+            // 범죄 유형과 값을 쌍으로 배열로 만듦
+            const crimes = [
+                { type: '폭력', value: crviol },
+                { type: '절도', value: crtheft },
+                { type: '성폭력', value: crsex }
+            ];
+
+            // 값을 기준으로 정렬, 값이 같을 경우 CRVIOL > CRTHEFT > CRSEX 우선 순위 적용
+            crimes.sort((a, b) => b.value - a.value || (a.type === '폭력' ? -1 : (a.type === '절도' ? -1 : 1)));
+
+            setCrimeRanks(crimes);
+        }
+    }, [selectedFeature]);
 
     const handleButtonClick = () => {
         navigate('/cpted-ai');
@@ -14,15 +35,14 @@ const CptedSuggest = ({ clickedAddress }) => {
             <h3>{clickedAddress}</h3>
             <p>에서 많이 일어나는 범죄</p>
             <div>
-                <p>1위: 절도</p>
-                <p>2위: 성폭력</p>                    
-                <p>3위: 폭력</p>
+                {crimeRanks.map((crime, index) => (
+                    <p key={index}>{index + 1}위: {crime.type}</p>
+                ))}
             </div>
             <h4>[CPTED 제안]</h4>
             <p>범죄를 줄이기 위해 가로등 설치를 제안합니다.</p>
             <button className="cpted-button" onClick={handleButtonClick}>지역 개선하기</button>
         </div>
-    
     );
 };
 
