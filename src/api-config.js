610dbd507483a9f573867a0f52e0702f50da28ac
jiyoung <gyjeong021@gmail.com>
2024-09-23 18:35:46 +0900

    [Fix] Cors 관련 코드 수정

--- src/api-config.js
+++ src/api-config.js
@@ -3,7 +3,7 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if( hostname === "localhost") {
-    backendHost = "http://safecid-be-env.eba-dbtmkx4p.ap-northeast-2.elasticbeanstalk.com/"; 
+    backendHost = "http://localhost:8000"; 
 }
 
 export const API_BASE_URL = `${backendHost}`;
