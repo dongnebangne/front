ee245850302608c8476bb2aee4359e693f5862d3
jiyoung <gyjeong021@gmail.com>
2024-10-01 17:25:36 +0900

    [Fix] 백엔드 IP 주소 수정

--- src/api-config.js
+++ src/api-config.js
@@ -4,9 +4,9 @@ const hostname = window && window.location && window.location.hostname;
 
 if( hostname === "localhost") {
     //backendHost = "http://localhost:8000"; 
-    backendHost = "http://52.79.103.59"; 
+    backendHost = "http://52.78.97.23"; 
 } else {
-    backendHost = "http://52.79.103.59"; 
+    backendHost = "http://52.78.97.23"; 
 }
 
 export const API_BASE_URL = `${backendHost}`;
