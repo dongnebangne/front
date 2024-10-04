644d6585e03be88c4d5796ad7bc8a29d7951959f
Jiyoung <gyjeong021@gmail.com>
2024-10-04 17:49:41 +0900

    Update api-config.js

--- src/api-config.js
+++ src/api-config.js
@@ -4,9 +4,9 @@ const hostname = window && window.location && window.location.hostname;
 
 if( hostname === "localhost") {
     //backendHost = "http://localhost:8000"; 
-    backendHost = "http://43.201.102.175"; 
+    backendHost = "http://3.35.119.183"; 
 } else {
-    backendHost = "http://43.201.102.175"; 
+    backendHost = "http://3.35.119.183"; 
 }
 
 export const API_BASE_URL = `${backendHost}`;
