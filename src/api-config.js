fbc0ae835fa32e21c14086baaad52cf3e8ce510f
Yewon0106 <yooyy0106@naver.com>
2024-10-07 14:15:14 +0900

    로컬확인용

--- src/api-config.js
+++ src/api-config.js
@@ -3,8 +3,8 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if( hostname === "localhost") {
-    //backendHost = "http://localhost:8000"; 
-    backendHost = "http://43.201.102.175"; 
+    backendHost = "http://localhost:8000"; 
+    //backendHost = "http://43.201.102.175"; 
 } else {
     backendHost = "http://43.201.102.175"; 
 }
