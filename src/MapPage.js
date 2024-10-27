971ba9c76797b84be09cc886acf0ba24854aac37
nongdamgom1over <phs1289@gmail.com>
2024-10-28 02:42:43 +0900

    10/28 오류가 좀 있네..

--- src/MapPage.js
+++ src/MapPage.js
@@ -1,12 +1,11 @@
 import React, { useRef, useEffect, useState, useContext } from 'react';
 import 'ol/ol.css';
-import { Map, View } from 'ol';
+import { Map, View, Overlay } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
-import TileWMS from 'ol/source/TileWMS';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
-import { fromLonLat, toLonLat } from 'ol/proj'; // toLonLat 추가
+import { fromLonLat, toLonLat } from 'ol/proj';
 import { Style, Fill } from 'ol/style';
 import { GeoJSON } from 'ol/format';
 import { AddressContext } from './AddressContext';
@@ -16,13 +15,15 @@ import CptedSuggest from './CptedSuggest';
 const MapPage = ({ layers, coordinates, geojsonVisible, setGeojsonVisible }) => {
   const mapRef = useRef(null);
   const mapElement = useRef();
+  const tooltipRef = useRef(null); // Tooltip element
+  const overlayRef = useRef(null); // Overlay for tooltip
   const apiKey = process.env.REACT_APP_WMTS_MAP_API_KEY;
   const { clickedAddress, setClickedAddress } = useContext(AddressContext);
   const [userCoordinates, setUserCoordinates] = useState(null);
-  const [geojsonData, setGeojsonData] = useState(null); // 처음 로드한 데이터를 저장
-  const geojsonLayerRef = useRef(null);  // 자취촌 레이어 참조
-  const wmsLayersRef = useRef([]);  // WMS 레이어들을 저장
-  const [selectedFeature, setSelectedFeature] = useState(null);  // 선택된 feature 상태 추가
+  const [geojsonData, setGeojsonData] = useState(null);
+  const geojsonLayerRef = useRef(null);
+  const wmsLayersRef = useRef([]);
+  const [selectedFeature, setSelectedFeature] = useState(null);
 
   // 사용자 위치 가져오기
   useEffect(() => {
@@ -69,7 +70,7 @@ const MapPage = ({ layers, coordinates, geojsonVisible, setGeojsonVisible }) => 
         fetch('https://smallgeojson.s3.ap-northeast-2.amazonaws.com/TestGrid3.geojson')
           .then((response) => response.json())
           .then((data) => {
-            setGeojsonData(data);  // 데이터를 상태에 저장
+            setGeojsonData(data);
           });
       }
 
@@ -79,25 +80,33 @@ const MapPage = ({ layers, coordinates, geojsonVisible, setGeojsonVisible }) => 
         const clickedFeature = map.forEachFeatureAtPixel(event.pixel, (feature) => feature);
 
         if (clickedFeature) {
-          const lonLat = toLonLat(clickedCoordinate);  // toLonLat 함수 사용
+          const lonLat = toLonLat(clickedCoordinate);
           const address = await getAddress(lonLat[1], lonLat[0]);
           setClickedAddress(address);
-          setSelectedFeature(clickedFeature);  // 선택된 feature 저장
+          setSelectedFeature(clickedFeature);
         }
       });
+
+      // 툴팁 오버레이 설정
+      const overlay = new Overlay({
+        element: tooltipRef.current,
+        offset: [10, 0],
+        positioning: 'bottom-left',
+      });
+      overlayRef.current = overlay;
+      map.addOverlay(overlay);
 
       return () => map.setTarget(undefined);
     }
   }, [apiKey, coordinates, userCoordinates]);
 
-  // 스타일 변경 로직 (옵션에 따라 GeoJSON 레이어의 스타일 업데이트)
+  // 스타일 변경 로직
   useEffect(() => {
     if (geojsonData && geojsonVisible) {
       const source = new VectorSource({
         features: new GeoJSON().readFeatures(geojsonData, { featureProjection: 'EPSG:3857' }),
       });
 
-      // GeoJSON 레이어 추가
       const geojsonLayer = new VectorLayer({
         source: source,
         style: function (feature) {
@@ -105,113 +114,88 @@ const MapPage = ({ layers, coordinates, geojsonVisible, setGeojsonVisible }) => 
 
           let fillColor;
           switch (value) {
-            case 1:
-              fillColor = 'rgba(255, 255, 255, 0.0)';
-              break;
-            case 2:
-              fillColor = 'rgba(255, 255, 178, 0.4)';
-              break;
-            case 3:
-              fillColor = 'rgba(254, 232, 139, 0.4)';
-              break;
-            case 4:
-              fillColor = 'rgba(254, 209, 101, 0.4)';
-              break;
-            case 5:
-              fillColor = 'rgba(253, 183, 81, 0.4)';
-              break;
-            case 6:
-              fillColor = 'rgba(253, 155, 67, 0.4)';
-              break;
-            case 7:
-              fillColor = 'rgba(250, 122, 53, 0.4)';
-              break;
-            case 8:
-              fillColor = 'rgba(244, 86, 41, 0.4)';
-              break;
-            case 9:
-              fillColor = 'rgba(234, 52, 32, 0.4)';
-              break;
-            case 10:
-              fillColor = 'rgba(211, 26, 35, 0.4)';
-              break;
-            case 11:
-              fillColor = 'rgba(189, 0, 38, 0.4)';
-              break;
-            default:
-              fillColor = 'rgba(0, 0, 0, 0.0)';
+            case 1: fillColor = 'rgba(255, 255, 255, 0.0)'; break;
+            case 2: fillColor = 'rgba(255, 255, 178, 0.4)'; break;
+            case 3: fillColor = 'rgba(254, 232, 139, 0.4)'; break;
+            case 4: fillColor = 'rgba(254, 209, 101, 0.4)'; break;
+            case 5: fillColor = 'rgba(253, 183, 81, 0.4)'; break;
+            case 6: fillColor = 'rgba(253, 155, 67, 0.4)'; break;
+            case 7: fillColor = 'rgba(250, 122, 53, 0.4)'; break;
+            case 8: fillColor = 'rgba(244, 86, 41, 0.4)'; break;
+            case 9: fillColor = 'rgba(234, 52, 32, 0.4)'; break;
+            case 10: fillColor = 'rgba(211, 26, 35, 0.4)'; break;
+            case 11: fillColor = 'rgba(189, 0, 38, 0.4)'; break;
+            default: fillColor = 'rgba(0, 0, 0, 0.0)';
           }
 
           return new Style({
-            fill: new Fill({
-              color: fillColor,
-            }),
+            fill: new Fill({ color: fillColor }),
           });
         },
       });
 
-      // API 레이어가 추가된 경우 기존 WMS 레이어 제거
-      wmsLayersRef.current.forEach((layer) => {
-        mapRef.current.removeLayer(layer);
-      });
-      wmsLayersRef.current = [];
-
-      // 자취촌 레이어를 관리하고 업데이트
       if (geojsonLayerRef.current) {
         mapRef.current.removeLayer(geojsonLayerRef.current);
       }
-      geojsonLayerRef.current = geojsonLayer;  // 자취촌 레이어를 참조로 저장
+      geojsonLayerRef.current = geojsonLayer;
       mapRef.current.addLayer(geojsonLayer);
-    } else {
-      // 자취촌 레이어 숨기기
-      if (geojsonLayerRef.current) {
-        mapRef.current.removeLayer(geojsonLayerRef.current);
-      }
+
+      // 툴팁 표시
+      mapRef.current.on('pointermove', (event) => {
+        const feature = mapRef.current.forEachFeatureAtPixel(event.pixel, (f) => f);
+        if (feature) {
+          const crall = feature.get('CRALL');
+          const crviol = feature.get('CRVIOL');
+          const crtheft = feature.get('CRTHEFT');
+          const crsex = feature.get('CRSEX');
+
+          // 가장 높은 위험 범죄 유형 결정
+          let crimeMessage = '';
+          let crimeelement = ''; // crimeelement 변수를 선언합니다.
+
+          const highestCrime = Math.max(crall, crviol, crtheft, crsex);
+          switch (highestCrime) {
+            case crall: 
+              crimeMessage = '전체범죄 발생 위험'; 
+              crimeelement = '건물 층수, 건물 연령, 건물 갯수, 건물 면적, 건물 연면적,<br/> 사교육 기관의 갯수, 버스정류장의 갯수,<br/> 지하철역과의 거리, 도로의 통과도, 도로의 전환비'; 
+              break;
+            case crviol: 
+              crimeMessage = '폭력 발생 위험'; 
+              crimeelement = '건물 층수, 건물 연령, 건물 갯수, 건물 면적, 건물 연면적,<br/> 휴게음식점의 갯수, 지하철역과의 거리,<br/> 도로의 통과도, 도로의 전환비'; 
+              break;
+            case crtheft: 
+              crimeMessage = '절도 발생 위험'; 
+              crimeelement = '건물 층수, 건물 갯수, 건물 면적, 건물 연면적,<br/> 금융보험기관의 갯수, 버스정류장의 갯수,<br/> 도로의 통과도, 도로의 전환비'; 
+              break;
+            case crsex: 
+              crimeMessage = '성폭력 발생 위험'; 
+              crimeelement = '숙박기관의 갯수, CCTV의 갯수,<br/> 도로의 통과도'; 
+              break;
+            default: 
+              crimeMessage = '';
+          }
+          
+
+          tooltipRef.current.innerHTML = `
+            <div>
+              <strong>${crimeMessage}</strong>
+              <p>${crimeelement}가 영향을 미칩니다.</p>
+            </div>
+          `;
+          overlayRef.current.setPosition(event.coordinate);
+        } else {
+          overlayRef.current.setPosition(undefined);
+        }
+      });
+    } else if (geojsonLayerRef.current) {
+      mapRef.current.removeLayer(geojsonLayerRef.current);
     }
   }, [geojsonData, geojsonVisible]);
-
-  // WMS 레이어 추가 로직 (API 요청에 따라 레이어를 추가)
-  useEffect(() => {
-    const addWMSLayers = (layers) => {
-      // 자취촌 레이어가 활성화되어 있다면 제거
-      if (geojsonLayerRef.current) {
-        mapRef.current.removeLayer(geojsonLayerRef.current);
-      }
-
-      // 이전에 추가된 WMS 레이어가 있으면 제거
-      wmsLayersRef.current.forEach((layer) => {
-        mapRef.current.removeLayer(layer);
-      });
-      wmsLayersRef.current = [];  // WMS 레이어 배열 초기화
-
-      layers.forEach((layer) => {
-        const wmsLayer = new TileLayer({
-          source: new TileWMS({
-            url: layer.serverUrl,
-            params: {
-              'LAYERS': layer.layername,
-              'STYLES': layer.styles,
-              'FORMAT': 'image/png',
-              'TRANSPARENT': true,
-            },
-            serverType: 'geoserver',
-            transition: 0,
-          }),
-          opacity: 0.7,
-        });
-        mapRef.current.addLayer(wmsLayer);
-        wmsLayersRef.current.push(wmsLayer);  // 추가된 레이어를 배열에 저장
-      });
-    };
-
-    if (layers && layers.length > 0) {
-      addWMSLayers(layers);
-    }
-  }, [layers]);
 
   return (
     <div>
       <div ref={mapElement} style={{ width: '100%', height: '100vh' }}></div>
+      <div ref={tooltipRef} className="tooltip" style={{ background: 'rgba(0, 0, 0, 0.6)', color: '#fff', padding: '5px', borderRadius: '3px' }}></div>
       {clickedAddress && selectedFeature && (
         <CptedSuggest clickedAddress={clickedAddress} selectedFeature={selectedFeature} />
       )}
